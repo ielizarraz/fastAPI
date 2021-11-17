d357b765d24899b62fd5aafbf2387afbb5771683
ielizarraz <iemonkey99@yahoo.com>
2021-11-16 19:37:33 -0800

    Added SQLAlchemy

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 
-from fastapi import FastAPI , Response , status
+from fastapi import FastAPI , Response , status , Depends
 from fastapi.exceptions import HTTPException
 from fastapi.param_functions import Body
 from pydantic import BaseModel
@@ -8,14 +8,20 @@ from random import randrange
 import time
 import psycopg2
 from psycopg2.extras import RealDictCursor
+from . import models
+from .database import engine, get_db
+from sqlalchemy.orm import Session
 
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-class Post(BaseModel):
+
+""" class Post(BaseModel):
     title: str 
     content: str
-    published: bool = True
+    published: bool = True """
 while True:
     
     try: 
@@ -30,21 +36,18 @@ while True:
 
 
 #POSTS CRUD 
-my_posts = [{"title" : "title of post 1" , "content" : "content of post 1", "id": 1}, {"title" : "title of post 2" , "content" : "content of post 2", "id": 2}]
 
 
-def find_post(id):
-    for post in my_posts:
-        if id == post["id"]:
-            return post
-def find_post_index(id):
-    for i,post in enumerate(my_posts):
-        if id == post["id"]:
-            return i
+
+@app.get("/sqlalchemy")
+def test_posts(db: Session = Depends(get_db)):
+    posts =  db.query(models.Post).all()
+    return {"data": posts}
+
 
 #Create New Post
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_posts(post: Post):
+def create_posts():
     cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""", (post.title, post.content, post.published))
     new_post = cursor.fetchone()
     
@@ -73,15 +76,15 @@ def read_post(id: int):
     
 #Update post
 @app.put("/posts/{id}")
-def update_post(post: Post, id: int):
+def update_post( id: int):
     cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""", (post.title, post.content, post.published, str(id)))
     updated_post = cursor.fetchone()
     conn.commit()
     if not updated_post: 
         raise HTTPException(status.HTTP_404_NOT_FOUND, f"post with id: {id} was not found")
     return {"data: ": updated_post}
-#Delete a specific post 
 
+#Delete a specific post 
 @app.delete('/posts/{id}', status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int):
     cursor.execute(""" DELETE FROM posts WHERE id = %s RETURNING *""", (str(id),))
